19631be76fc99b91005c7cd3a16e3a1fad028c65
‘minzung’ <108450681+minzung@users.noreply.github.com>
2023-04-09 01:30:06 +0900

    Jwt 설정 수정

--- src/main/java/hansung/capstone/jwt/AuthResponse.java
+++ src/main/java/hansung/capstone/jwt/AuthResponse.java
@@ -1,27 +1,15 @@
 package hansung.capstone.jwt;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class AuthResponse {
+
     private String accessToken;
     private String refreshToken;
 
-    public AuthResponse(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public void setRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
 }
