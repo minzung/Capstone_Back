580c9abef71f54da2bacb2bdf257d27989b20673
‘minzung’ <108450681+minzung@users.noreply.github.com>
2023-04-10 13:26:40 +0900

    Request 추가

---
+++ src/main/java/hansung/capstone/dto/request/UpdateEmailRequest.java
@@ -0,0 +1,12 @@
+package hansung.capstone.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateEmailRequest {
+
+    private String email;
+
+    private String password;
+
+}
---
+++ src/main/java/hansung/capstone/dto/request/UpdatePasswordRequest.java
@@ -0,0 +1,10 @@
+package hansung.capstone.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UpdatePasswordRequest {
+
+    private String password;
+
+}
