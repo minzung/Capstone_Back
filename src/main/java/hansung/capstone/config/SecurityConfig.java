7fdddd5d9bafc67214d65ce564c4e4bc48356de3
‘minzung’ <108450681+minzung@users.noreply.github.com>
2023-04-10 18:17:29 +0900

    antMatchers lecture 코드 추가

--- src/main/java/hansung/capstone/config/SecurityConfig.java
+++ src/main/java/hansung/capstone/config/SecurityConfig.java
@@ -11,12 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
