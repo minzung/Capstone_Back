120e4a7b720fd68071078aab66ec1eaec70c0738
‘minzung’ <108450681+minzung@users.noreply.github.com>
2023-04-18 00:07:30 +0900

    함수명 수정

--- src/main/java/hansung/capstone/dao/FreeBoardDAO.java
+++ src/main/java/hansung/capstone/dao/FreeBoardDAO.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FreeBoardDAO extends JpaRepository<FreeBoardDTO, Integer>  {
 
-    FreeBoardDTO findByFreeboardId(int freeboardId);
+    FreeBoardDTO findById(int boardId);
 
 }
