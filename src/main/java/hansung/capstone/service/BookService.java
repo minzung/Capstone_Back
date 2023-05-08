2baacff8627240c0d4b665b737ef16dd80f9e788
‘minzung’ <108450681+minzung@users.noreply.github.com>
2023-05-08 20:59:48 +0900

    경로 변경

--- src/main/java/hansung/capstone/service/BookService.java
+++ src/main/java/hansung/capstone/service/BookService.java
@@ -23,7 +23,7 @@ public class BookService {
 
         if (base64Image != null && !base64Image.isEmpty()) {
             // 저장할 디렉토리 지정
-            String uploadDir = System.getProperty("user.dir") + "/src/main/resources/static/freeboard/";
+            String uploadDir = System.getProperty("user.dir") + "/src/main/resources/static/book/";
             // 고유한 파일 이름 생성
             String fileName = "image_" + bookDTO.getStudentId() + System.currentTimeMillis() + ".png";
             Path path = Paths.get(uploadDir + fileName);
