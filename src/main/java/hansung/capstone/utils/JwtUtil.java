ca9b8511b4d808c60bb7408279f5d3217e6690b1
‘minzung’ <108450681+minzung@users.noreply.github.com>
2023-05-13 22:06:48 +0900

    관리자 토큰 추가

--- src/main/java/hansung/capstone/utils/JwtUtil.java
+++ src/main/java/hansung/capstone/utils/JwtUtil.java
@@ -7,23 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
 
     private static final String SECRET_KEY = generateSecretKey(); // 시크릿 키
     private static final int SECRET_KEY_LENGTH = 32; // 256비트, 32바이트
-    private long ACCESS_EXPIRATION_TIME = 3600000L; // 1 hour
-    private long REFRESH_EXPIRATION_TIME = 86400000L * 30; // 30 day
+    private static long ACCESS_EXPIRATION_TIME = 3600000L; // 1 hour
+    private static long REFRESH_EXPIRATION_TIME = 86400000L * 30; // 30 day
 
     public String createAccessToken(String studentId) {
         Claims claims = Jwts.claims().setSubject(studentId);
-        claims.put("aud", studentId);
-        if(studentId.equals("2071243")) {
-            claims.put("roles", "admin");
-        } else {
-            claims.put("roles", "user");
-        }
 
         Date now = new Date();
         Date validity = new Date(now.getTime() + ACCESS_EXPIRATION_TIME);
@@ -38,12 +34,6 @@ public class JwtUtil {
 
     public String createRefreshToken(String studentId) {
         Claims claims = Jwts.claims().setSubject(studentId);
-        claims.put("aud", studentId);
-        if(studentId.equals("2071243")) {
-            claims.put("roles", "admin");
-        } else {
-            claims.put("roles", "user");
-        }
 
         Date now = new Date();
         Date validity = new Date(now.getTime() + REFRESH_EXPIRATION_TIME);
@@ -99,6 +89,47 @@ public class JwtUtil {
         byte[] randomBytes = new byte[SECRET_KEY_LENGTH];
         new SecureRandom().nextBytes(randomBytes);
         return Base64.getUrlEncoder().withoutPadding().encodeToString(randomBytes);
+    }
+
+    // ------------------------------ 관리자 -----------------------------------
+    public static String generateManagerJwtToken(String code, String studentId) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("sub", code);
+        claims.put("aud", studentId);
+        if(studentId.equals("2071243") || studentId.equals("김민서")){
+            claims.put("roles", "admin");
+        }else{
+            claims.put("roles", "user");
+        }
+
+
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + ACCESS_EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
+
+    public static Claims getJwtTokenInfo(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims;
+    }
+
+    // JWT 검증
+    public static boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 }
